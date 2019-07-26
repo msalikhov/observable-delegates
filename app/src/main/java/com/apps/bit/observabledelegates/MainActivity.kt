47740cf3ec2fb4e8c8a27ccef640747c11ad4938
Marat Salikhov <unk13b16@yandex.ru>
2019-07-26 20:46:11 +0400

    androidx

--- app/src/main/java/com/apps/bit/observabledelegates/MainActivity.kt
+++ app/src/main/java/com/apps/bit/observabledelegates/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.apps.bit.observabledelegates
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
